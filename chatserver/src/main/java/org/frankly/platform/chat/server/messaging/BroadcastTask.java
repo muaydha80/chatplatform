6a7b0258c97231b9dddb47dd67f864f6beb6a0d4
Ahmed Muaydh <ahmedm1@gmail.com>
2015-04-09 18:07:29 -0700

    Update BroadcastTask.java

--- chatserver/src/main/java/org/frankly/platform/chat/server/messaging/BroadcastTask.java
+++ chatserver/src/main/java/org/frankly/platform/chat/server/messaging/BroadcastTask.java
@@ -1,48 +1,70 @@
 package org.frankly.platform.chat.server.messaging;
 
-import java.io.IOException;
+import org.frankly.platform.chat.server.ChatServer;
+
+import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.websocket.Session;
 
 /**
- * 
+ *
  * @author Ahmed
  *
  */
-class BroadcastTask implements Runnable{
-	private ChatRoomSessionsManager chatRoomSessionsManager = 
-			ChatRoomSessionsManager.getInstance();
-	private Session currentSession;
-	private String broadcastMessage;
-	private String room;
-	
-	/**
-	 * The task responsbile to send a broadcast message
-	 * @param currentSession
-	 * @param broadcastMessage
-	 * @param room
-	 */
-	public BroadcastTask(Session currentSession, String broadcastMessage, String room){
-		this.currentSession = currentSession;
-		this.broadcastMessage = broadcastMessage;
-		this.room = room;
-	}
-	
+class BroadcastTask implements Runnable {
+  private static Logger logger = Logger.getLogger(ChatServer.class.getName());
 
-	/**
-	 * The run method for task(thread) to send messages in async way
-	 */
-	public void run(){
-		List<Session> memberSessionList = chatRoomSessionsManager.getMemberSessionList(this.room);
-		for(Session session: memberSessionList){
-			if(session != this.currentSession){
-				try {
-					session.getBasicRemote().sendText(this.broadcastMessage);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+  private ChatRoomSessionsManager chatRoomSessionsManager = ChatRoomSessionsManager.getInstance();
+  private Session currentSession;
+  private String broadcastMessage;
+  private String room;
+
+  /**
+   * The task responsbile to send a broadcast message
+   *
+   * @param currentSession
+   * @param broadcastMessage
+   * @param room
+   */
+  public BroadcastTask(Session currentSession, String broadcastMessage, String room) {
+    this.currentSession = currentSession;
+    this.broadcastMessage = broadcastMessage;
+    this.room = room;
+  }
+
+
+  /**
+   * The run method for task(thread) to send messages in async way
+   */
+  @Override
+  public void run() {
+    List<Session> memberSessionList = chatRoomSessionsManager.getMemberSessionList(this.room);
+
+    if(memberSessionList == null)
+      return;
+    
+    Iterator<Session> iterator = memberSessionList.iterator();
+    while (iterator.hasNext()) {
+      Session session = iterator.next();
+
+      // Check if session is already closed then remove it
+      if (!session.isOpen()) {
+        continue;
+      }
+
+      if (session != this.currentSession) {
+        try {
+          //synchronized around session because API only allows one send 
+          synchronized(session){
+            session.getBasicRemote().sendText(this.broadcastMessage);
+          }
+        } catch (Exception e) {
+          logger.log(Level.INFO, "Failed to broadcast message " + e.getMessage(), e);
+        }
+      }
+    }
+  }
 }
