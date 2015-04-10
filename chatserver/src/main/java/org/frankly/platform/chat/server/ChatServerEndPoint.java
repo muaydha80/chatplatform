eaec2414ce6adc63b0d3711a3cc06b3441997b57
Ahmed Muaydh <ahmedm1@gmail.com>
2015-04-09 18:07:06 -0700

    Update ChatServerEndPoint.java

--- chatserver/src/main/java/org/frankly/platform/chat/server/ChatServerEndPoint.java
+++ chatserver/src/main/java/org/frankly/platform/chat/server/ChatServerEndPoint.java
@@ -11,92 +11,104 @@ import javax.websocket.server.ServerEndpoint;
 import org.frankly.platform.chat.server.messaging.ChatRoomSessionsManager;
 import org.frankly.platform.chat.server.messaging.MessageBroadcastEngine;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * 
+ *
  * @author muaydha
  *
  */
 @ServerEndpoint(value = "/{room}")
 public class ChatServerEndPoint {
 
-	private static Logger logger = Logger.getLogger(ChatServerEndPoint.class.getName());
-	private MessageBroadcastEngine broadcastManager = new MessageBroadcastEngine();
-	private ChatRoomSessionsManager chatRoomSessionsManager = ChatRoomSessionsManager
-			.getInstance();
+  private static Logger logger = Logger.getLogger(ChatServerEndPoint.class.getName());
+  private MessageBroadcastEngine broadcastManager = new MessageBroadcastEngine();
+  private ChatRoomSessionsManager chatRoomSessionsManager = ChatRoomSessionsManager.getInstance();
 
-	/**
-	 * This method is called to open websocket session
-	 * @param session
-	 * @param room
-	 */
-	@OnOpen
-	public void onOpen(Session session, @PathParam("room") String room) {
-		session.getUserProperties().put("room", room);
-		String message = getClientAddress(session) + " joined the party!";
-		logger.log(Level.INFO, message);
-		publishMessage(session, message, room);
-		chatRoomSessionsManager.addMemberToRoom(session, room);
-	}
+  /**
+   * This method is called to open websocket session
+   *
+   * @param session
+   * @param room
+   */
+  @OnOpen
+  public void onOpen(Session session, @PathParam("room") String room) {
+    session.getUserProperties().put("room", room);
+    String message = getClientAddress(session) + " joined the party!";
+    logger.log(Level.INFO, message);
+    publishMessage(session, message, room);
+    chatRoomSessionsManager.addMemberToRoom(session, room);
+  }
 
-	/**
-	 * This method is called when server receives a message
-	 * @param message
-	 * @param session
-	 */
-	@OnMessage
-	public void onMessage(String message, Session session) {
-		String room = (String) session.getUserProperties().get("room");
-		logger.log(Level.INFO, "Received message: " + message);
-		publishMessage(session, message, room);
+  /**
+   * This method is called when server receives a message
+   *
+   * @param message
+   * @param session
+   */
+  @OnMessage
+  public void onMessage(String message, Session session) {
+    String room = (String) session.getUserProperties().get("room");
+    logger.log(Level.INFO, "Received message: " + message);
+    publishMessage(session, message, room);
 
-	}
-	/**
-	 * This method is called when server receives a message
-	 * @param message
-	 * @param session
-	 */
-	@OnMessage
-	public void onMessage(InputStream in, Session session) {
-		logger.log(Level.INFO, "Close session: " + getClientAddress(session));
-		session.close();
-	}
-	/**
-	 * This method is called when websocket is closed
-	 * @param session
-	 * @param room
-	 */
-	@OnClose
-	public void onClose(Session session, @PathParam("room") String room) {
-		chatRoomSessionsManager.removeMemberFromRoom(session, room);
-		String message = getClientAddress(session) + " passed out...";
-		logger.log(Level.INFO, message);
-		publishMessage(session, message, room);
+  }
 
-	}
+  
+  /**
+   * This method is called when server receives a binary message
+   *
+   * @param message
+   * @param session
+   */
+  @OnMessage
+  public void onMessage(InputStream message, Session session) {
+    logger.log(Level.INFO, "Received Binary message. Closing client: "+ getClientAddress(session) );
+    try {
+      session.close();
+    } catch (IOException e) {
+      logger.log(Level.INFO, "Failed to close session " + e.getMessage(), e);
+    }
 
-	/**
-	 * Get client address
-	 * @param session
-	 * @return
-	 */
-	public String getClientAddress(Session session) {
-		return session.getUserProperties()
-						.get("javax.websocket.endpoint.remoteAddress")
-						.toString();
-	}
+  }
+  
+  /**
+   * This method is called when websocket is closed
+   *
+   * @param session
+   * @param room
+   */
+  @OnClose
+  public void onClose(Session session, @PathParam("room") String room) {
+    chatRoomSessionsManager.removeMemberFromRoom(session, room);
+    String message = getClientAddress(session) + " passed out...";
+    logger.log(Level.INFO, message);
+    publishMessage(session, message, room);
 
-	/**
-	 * Publish Message to other websockets
-	 * @param session
-	 * @param message
-	 * @param room
-	 */
-	private void publishMessage(Session session, String message, String room) {
-		broadcastManager.broadcast(session, message, room);
-	}
+  }
+
+  /**
+   * Get client address
+   *
+   * @param session
+   * @return client Address
+   */
+  public String getClientAddress(Session session) {
+    return session.getUserProperties().get("javax.websocket.endpoint.remoteAddress").toString();
+  }
+
+  /**
+   * Publish Message to other websockets
+   *
+   * @param session
+   * @param message
+   * @param room
+   */
+  private void publishMessage(Session session, String message, String room) {
+    broadcastManager.broadcast(session, message, room);
+  }
 
 }
